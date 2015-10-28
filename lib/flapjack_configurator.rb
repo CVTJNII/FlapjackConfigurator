65ad4bed07e02a9eb14e71d9bac378cab9cde8d3
Tom Noonan II <thomas.noonan@corvisa.com>
2015-10-27 21:10:14 -0600

    Fix runtime bugs

--- lib/flapjack_configurator.rb
+++ lib/flapjack_configurator.rb
@@ -8,7 +8,7 @@ require 'flapjack_configurator/version'
 module FlapjackConfigurator
   # Method to configure flapjack
   def self.configure_flapjack(config, api_base_url = 'http://127.0.0.1:3081', logger = nil)
-    Flapjack::Diner.base_uri('http://127.0.0.1:3081')
+    Flapjack::Diner.base_uri(api_base_url)
     unless logger
       logger = Logger.new(STDOUT)
       logger.level = Logger::INFO
