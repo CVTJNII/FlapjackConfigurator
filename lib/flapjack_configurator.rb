89243245a0767ba23985e935a8f31021c894c9de
Tom Noonan II <thomas.noonan@corvisa.com>
2015-10-27 20:47:11 -0600

    Add a version method

--- lib/flapjack_configurator.rb
+++ lib/flapjack_configurator.rb
@@ -3,8 +3,10 @@
 require 'flapjack-diner'
 require 'logger'
 require 'flapjack_configurator/flapjack_config'
+require 'flapjack_configurator/version'
 
 module FlapjackConfigurator
+  # Method to configure flapjack
   def self.configure_flapjack(config, api_base_url = 'http://127.0.0.1:3081', logger = nil)
     Flapjack::Diner.base_uri('http://127.0.0.1:3081')
     unless logger
@@ -18,5 +20,10 @@ module FlapjackConfigurator
     # This will update media, PD creds, notification rules, and entity associations
     #   as they're associated to the contact.
     return config_obj.update_contacts
+  end
+
+  # Simple helper to return the gem version
+  def self.version
+    return VERSION
   end
 end
